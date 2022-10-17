3f0a2b9da5d9d4b6ff8c804c4eaff43da2508ec0
Praveenshiva916 <114139922+Praveenshiva916@users.noreply.github.com>
2022-10-17 11:57:02 +0530

    push

---
+++ constra/constra.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.slider').bxSlider({
+      mode: 'fade',
+      captions: false,
+      controls: false,
+      startSlide: 1,
+      slideMargin: 9,
+      speed: 500,
+      slideWidth: 600
+      
+    
+    });
+  });
